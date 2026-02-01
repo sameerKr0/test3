244d88e5872611edf5f0931568d7b323b74db2f2
Sameer Kumar <sssameersingh0804@gmail.com>
2026-02-01 22:49:25 +0530

    Update Test3Application.java

--- src/main/java/com/test3/Test3Application.java
+++ src/main/java/com/test3/Test3Application.java
@@ -9,6 +9,7 @@ public class Test3Application {
 	public static void main(String[] args) {
 		String name="mike";
 		int x=100;
+		int y=10;
 		SpringApplication.run(Test3Application.class, args);
 	}
 
